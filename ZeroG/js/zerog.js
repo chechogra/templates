9ccca50b5ed34d70e86cbd552a3ef2c6a8f43ff5
chechogra <chechogra@gmail.com>
2015-09-19 00:32:13 -0500

    z logo animation second part

--- ZeroG/js/zerog.js
+++ ZeroG/js/zerog.js
@@ -1,23 +1,27 @@
 $( document ).ready(function() {
-  $("#zg_logo").animate({ width: "402px", height: "500px", marginTop: "-250px", marginLeft: "-201px"}, 1000, function() {
-    // Animation complete.
-  });
+	var logoWidth= $( window ).width() * 3;
+	var logoHeight= logoWidth * 1.2558081125;
+	var margin_right = "-" + $( window ).width() * 1.8;
+	var margin_bottom = "-" + logoHeight * 0.75;
+
+ 	TweenMax.to("#zg_logo", 0.5, {width:logoWidth,  height:logoHeight, display:"block", marginRight:margin_right, marginBottom:margin_bottom, ease:Power2.easeInOut});
   
+	
   $('#user_password').keypress(function (e) {
 	 var key = e.which;
 	 if(key == 13) {
-		TweenMax.to("#centered_login", 0.5, {alpha:0, display:"none", ease:Power2.easeInOut, onComplete: moveOutLogo});
+		TweenMax.to("#centered_login", 0.5, {alpha:0, display:"none", ease:Power2.easeInOut, onComplete: showZLogo});
 		return false;  
 	  }
    });
-   
-    function moveOutLogo(){
-		TweenMax.to("#zg_logo", 2, {opacity:0, rotation: 360, scale:2, display:"none", ease:Power2.easeInOut, onComplete: showZLogo});
-	}
 	
 	function showZLogo(){
+		
+		TweenMax.to("#zg_logo", 2, {width:"129px",  height:"160px", bottom:"auto", right:"auto", left:"50%", top:"50%", marginRight:0, 
+		marginBottom:0, marginTop:"-80px", marginLeft:"-190px", position:"fixed", rotation:"-180", ease:Power2.easeInOut});
+		
 		$("#centered_welcome").show();
-		TweenMax.from("#zg_logo_medium", 1, {rotationX:180, rotationY:180});
+		//TweenMax.from("#zg_logo_medium", 1, {rotationX:180, rotationY:180});
 		TweenMax.from("#welcome_txt", 1, {scale:0, ease:Power2.easeInOut, delay:1});
 		TweenMax.to("#colony_txt", 0.5, {text:{value:"TO THE ZERO G COLONY", delimiter:" "}, ease:Linear.easeNone, delay:2});
 		TweenMax.to("#welcome_p", 1, {text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut", ease:Linear.easeNone, delay:3});
