8151f139023ad4d5773d96bdf25f3526b9162e4c
chechogra <chechogra@gmail.com>
2015-09-19 11:17:03 -0500

    nav bar first part

--- ZeroG/js/zerog.js
+++ ZeroG/js/zerog.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
 	var margin_right = "-" + $( window ).width() * 1.8;
 	var margin_bottom = "-" + logoHeight * 0.75;
 
- 	TweenMax.to("#zg_logo", 0.5, {width:logoWidth,  height:logoHeight, display:"block", marginRight:margin_right, marginBottom:margin_bottom, ease:Power2.easeInOut});
+ 	TweenMax.to("#zg_logo", 2, {width:logoWidth,  height:logoHeight, display:"block", marginRight:margin_right, marginBottom:margin_bottom, ease:Power2.easeInOut});
   
 	
   $('#user_password').keypress(function (e) {
@@ -17,14 +17,60 @@ $( document ).ready(function() {
 	
 	function showZLogo(){
 		
-		TweenMax.to("#zg_logo", 2, {width:"129px",  height:"160px", bottom:"auto", right:"auto", left:"50%", top:"50%", marginRight:0, 
+		TweenMax.to("#zg_logo", 3, {width:"129px",  height:"160px", bottom:"auto", right:"auto", left:"50%", top:"50%", marginRight:0, 
 		marginBottom:0, marginTop:"-80px", marginLeft:"-190px", position:"fixed", rotation:"-180", ease:Power2.easeInOut});
 		
 		$("#centered_welcome").show();
-		//TweenMax.from("#zg_logo_medium", 1, {rotationX:180, rotationY:180});
-		TweenMax.from("#welcome_txt", 1, {scale:0, ease:Power2.easeInOut, delay:1});
-		TweenMax.to("#colony_txt", 0.5, {text:{value:"TO THE ZERO G COLONY", delimiter:" "}, ease:Linear.easeNone, delay:2});
-		TweenMax.to("#welcome_p", 1, {text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut", ease:Linear.easeNone, delay:3});
+		TweenMax.from("#welcome_txt", 1, {scale:0, ease:Power2.easeInOut, delay:3});
+		TweenMax.to("#colony_txt", 0.5, {text:{value:"TO THE ZERO G COLONY", delimiter:" "}, ease:Linear.easeNone, delay:4});
+		TweenMax.to("#welcome_p", 1, {text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut", ease:Linear.easeNone, delay:5});
+		TweenMax.to("#arrows_btn", 1, {display:"block", ease:Power0.easeNone, delay:6});
+		bindScrollEvent();
 	}
+	
+	function bindScrollEvent(){
+		//Firefox
+		 $(window).bind('DOMMouseScroll', function(e){
+			 if(e.originalEvent.detail > 0) {
+				 hideSplashScreen();
+			 }
+		
+			 //prevent page fom scrolling
+			 return false;
+		 });
+		
+		 //IE, Opera, Safari
+		 $(window).bind('mousewheel', function(e){
+			 if(e.originalEvent.wheelDelta < 0) {
+				 hideSplashScreen();
+			 }
+		
+			 //prevent page fom scrolling
+			 return false;
+		 });	
+	}
+	
+	function unBindScrollEvent(){
+		$(window).unbind('DOMMouseScroll');
+		$(window).unbind('mousewheel');
+	}
+	
+	$("#arrows_btn").click(function(e){
+		hideSplashScreen();
+	});
+	
+	function hideSplashScreen(){
+		unBindScrollEvent();
+		TweenMax.to("#centered_welcome", 0.5, {display:"none", top: "0", marginTop: "0px", ease:Power2.easeInOut});
+		TweenMax.to("#arrows_btn", 0.5, {display:"none", left:"100%", ease:Power2.easeInOut});
+		TweenMax.to("#zg_logo", 0.5, {display:"none", left:"0", ease:Power2.easeInOut, onComplete: showNavbar});
+		
+	}
+	
+	function showNavbar(){
+		TweenMax.to("#nav_bar", 0.5, {display:"block", ease:Power2.easeInOut});
+	}
+	
+	
   
 });
