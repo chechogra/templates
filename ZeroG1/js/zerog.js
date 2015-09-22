56f09a778b52fce0906fa15e5fd7cec8281051a9
chechogra <chechogra@gmail.com>
2015-09-22 16:01:09 -0500

    object structure implementation

--- ZeroG1/js/zerog.js
+++ ZeroG1/js/zerog.js
@@ -1,40 +1,45 @@
-$( document ).ready(function() {
-	var logoWidth= $( window ).width() * 13;
-	var logoHeight= logoWidth * 1.2502230152;
-	var margin_right = "-" + $( window ).width() * 6.8;
-	var margin_bottom = "-" + logoHeight * 0.858;
-	
- 	TweenMax.to("#zg_logo", 2, {width:logoWidth,  height:logoHeight, display:"block", marginRight:margin_right, marginBottom:margin_bottom, ease:Power2.easeInOut});
-  
-	
-  $('#user_password').keypress(function (e) {
-	 var key = e.which;
-	 if(key == 13) {
-		TweenMax.to("#centered_login", 0.5, {alpha:0, display:"none", ease:Power2.easeInOut, onComplete: showZLogo});
-		return false;  
-	  }
-   });
-	
-	function showZLogo(){
+var zeroG = {
+	initialize: function(){
+		var logoWidth= $( window ).width() * 13;
+		var logoHeight= logoWidth * 1.2502230152;
+		var margin_right = "-" + $( window ).width() * 6.8;
+		var margin_bottom = "-" + logoHeight * 0.858;
+ 		TweenMax.to("#zg_logo", 2, {width:logoWidth,  height:logoHeight, display:"block", marginRight:margin_right, marginBottom:margin_bottom, 
+		ease:Power2.easeInOut});
+		this.bindEvents();
+	},
+	bindEvents: function(){
+		var self = this;
+		$('#user_password').keypress(function (e) {
+		 var key = e.which;
+		 if(key == 13) {
+			self.performLogin();
+			return false;  
+		  }
+	    });
+	},
+	performLogin: function(){
+		var self = this;
+		TweenMax.to("#centered_login", 0.5, {alpha:0, display:"none", ease:Power2.easeInOut, onComplete: self.showZLogoAnimation(self)});
+	},
+	showZLogoAnimation: function(context){
 		TweenMax.to("#zg_logo", 1.3, {width:"129px",  height:"160px", bottom:"auto", right:"auto", left:"50%", top:"50%", marginRight:0, 
 		marginBottom:0, marginTop:"-80px", marginLeft:"-190px", position:"fixed",  ease:Power2.easeInOut});
 		TweenMax.to("#gradient_overlay", 1.3, { display:"none",  ease:Power2.easeInOut});
-
 		$("#centered_welcome").show();
 		TweenMax.from("#welcome_wrapper", 0.2, {alpha:0, ease:Power2.easeInOut, delay:1.3});
 		TweenMax.to("#centered_welcome", 0.2, {paddingTop:0, ease:Linear.easeNone, delay:1.3});
 		TweenMax.to("#welcome_p", 0.5, {text:"Lorem ipsum dolor sit consectetur", ease:Linear.easeNone, delay:1.6});
 		TweenMax.to("#arrows_btn", 1, {display:"block", opacity: 1, ease:Power0.easeNone, delay:2});
-		bindScrollEvent();
-	}
-	
-	function bindScrollEvent(){
+		context.bindSplashEvents();
+	},
+	bindSplashEvents: function(){
+		var self = this;
 		//Firefox
 		 $(window).bind('DOMMouseScroll', function(e){
 			 if(e.originalEvent.detail > 0) {
-				 hideSplashScreen();
+				 self.performSplashAnimation();
 			 }
-		
 			 //prevent page fom scrolling
 			 return false;
 		 });
@@ -42,25 +47,22 @@ $( document ).ready(function() {
 		 //IE, Opera, Safari
 		 $(window).bind('mousewheel', function(e){
 			 if(e.originalEvent.wheelDelta < 0) {
-				 hideSplashScreen();
+				 self.performSplashAnimation();
 			 }
-		
 			 //prevent page fom scrolling
 			 return false;
+		 });
+		 
+		 $("#arrows_btn").bind('click', function(e){
+			 self.performSplashAnimation();
 		 });	
-	}
-	
-	function unBindScrollEvent(){
+	},
+	unbindSplashEvents: function(){
 		$(window).unbind('DOMMouseScroll');
 		$(window).unbind('mousewheel');
-	}
-	
-	$("#arrows_btn").click(function(e){
-		hideSplashScreen();
-	});
-	
-	function hideSplashScreen(){
-		unBindScrollEvent();	
+	},
+	performSplashAnimation: function(){
+		this.unbindSplashEvents();	
 		var header_logo_position = ($("#container_reference").offset().left + 19) + "px";
 		
 		TweenMax.to("#arrows_btn", 0.7, {display:"none", alpha:0, ease:Power2.easeInOut});
@@ -73,5 +75,9 @@ $( document ).ready(function() {
 		TweenMax.to("#content_1", 0.5, {display:"block", opacity: 1, ease:Power2.easeInOut, delay:1});
 		TweenMax.to("#zg_logo_text", 0.5, {opacity: 1, ease:Power2.easeInOut, delay:1});
 	}
-  
+}
+
+
+$( document ).ready(function() {
+	zeroG.initialize();
 });
