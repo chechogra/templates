35d87a2aabe8e2bd0d4b3abb5d8e2600d83f95c1
chechogra <chechogra@gmail.com>
2015-10-06 00:45:47 -0500

    grunt inject

--- checkedup-template/Gruntfile.js
+++ checkedup-template/Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-wiredep');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-injector');
 
     grunt.initConfig({
         wiredep: {
@@ -9,12 +10,35 @@ module.exports = function(grunt) {
                 src: ['app/index.html']
             }
         },
+        injector: {
+            options: {
+                addRootSlash:false,
+                relative: true
+            },
+            local_dependencies: {
+                files: {
+                    'app/index.html': ['app/scripts/**/*.js', 'app/styles/**/*.css']
+                }
+            }
+        },
         watch: {
-            files: ['app/bower_components/*'],
-            tasks: ['wiredep']
+            bower:{
+                files: ['app/bower_components/*'],
+                tasks: ['wiredep']
+            },
+            scripts:{
+                files: ['app/scripts/**/*.js'],
+                tasks:['inject']
+            },
+            css:{
+                files:['app/styles/**/*.css'],
+                tasks:['inject']
+            }
+
         }
     });
 
     grunt.registerTask('default', ['wiredep']);
+    grunt.registerTask('inject', ['injector']);
     grunt.registerTask('changes', ['watch']);
 };
