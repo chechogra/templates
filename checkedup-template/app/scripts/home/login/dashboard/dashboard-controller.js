09d72968aa803e2988db1cdcacf7189284b6fff6
chechogra <chechogra@gmail.com>
2015-10-11 21:39:06 -0500

    Custom toast implementation

--- checkedup-template/app/scripts/home/login/dashboard/dashboard-controller.js
+++ checkedup-template/app/scripts/home/login/dashboard/dashboard-controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('checkedUpApp')
-    .controller('DashboardCtrl', function ($scope, $stateParams) {
+    .controller('DashboardCtrl', function ($scope, $mdToast, $stateParams) {
 
         var videoPlayer = document.getElementById("video_player");
         $scope.showVideo = true;
@@ -13,6 +13,21 @@ angular.module('checkedUpApp')
             videoPlayer.onloadedmetadata = function () {
                 $scope.showVideo = true;
             };
+        };
+
+        $scope.showActionToast = function() {
+            var toast = $mdToast.simple()
+                .content('Action Toast!')
+                .action('OK')
+                .highlightAction(false)
+                .hideDelay(false)
+                .parent(document.getElementById("content_dashboard"))
+                .position('bottom');
+            $mdToast.show(toast).then(function(response) {
+                if ( response == 'ok' ) {
+                    alert('You clicked \'OK\'.');
+                }
+            });
         };
 
 
