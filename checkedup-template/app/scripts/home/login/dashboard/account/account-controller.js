f83829a1119e8f0ec1b108cbd4944224686767aa
chechogra <chechogra@gmail.com>
2015-10-12 23:07:23 -0500

    delete account modal

--- checkedup-template/app/scripts/home/login/dashboard/account/account-controller.js
+++ checkedup-template/app/scripts/home/login/dashboard/account/account-controller.js
@@ -1,14 +1,33 @@
 'use strict';
 
 angular.module('checkedUpApp')
-    .controller('AccountCtrl', function ($scope) {
+    .controller('AccountCtrl', function ($scope, $mdDialog) {
 
         $scope.init = function(){
 
         };
 
-        $scope.deletePatient = function(){
+        $scope.showDeleteAccoount = function($event) {
+            //TODO fix prevent default (only works the first time)
+            $event.preventDefault();
+            $mdDialog.show({
+                controller: 'AccountCtrl',
+                templateUrl: 'scripts/home/login/dashboard/account/delete/delete-patient.html',
+                parent: angular.element(document.body),
+                focusOnOpen: false,
+                scope: $scope,
+                targetEvent: $event,
+                clickOutsideToClose:true,
+                escapeToClose: true
+            });
+        };
 
+        $scope.deletePatient = function($event){
+            $mdDialog.hide();
+        };
+
+        $scope.cancel = function($event){
+            $mdDialog.hide();
         };
 
         $scope.updatePatient = function(accountForm){
